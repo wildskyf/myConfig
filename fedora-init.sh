8d82e8d9e29d36850e76914ec169efe6380d1bd6
Wildsky Fann <poppin.wildsky+gh@gmail.com>
2016-05-08 20:00:01 +0800

    Not use sudo

--- fedora-init.sh
+++ fedora-init.sh
@@ -1,60 +1,56 @@
 #!/bin/sh
 
 # Are you root?
-if [ "$(id -u)" == 0 ]; then
-  echo "You should not run this script directly as root."
-  exit 1
-elif ! sudo -v; then
-  echo "Please configure sudo correctly before running this script."
-  exit 1
+if [ "$(id -u)" != 0 ]; then
+  echo "You should run this script directly as root."
 fi
 
-sudo echo "fastestmirror=true" >> /etc/dnf/dnf.conf
+echo "fastestmirror=true" >> /etc/dnf/dnf.conf
 
 # Repos
-sudo dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-stable.noarch.rpm
-sudo dnf install http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-stable.noarch.rpm
-sudo dnf copr enable nibbler/nodejs
+dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-stable.noarch.rpm
+dnf install http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-stable.noarch.rpm
+dnf copr enable nibbler/nodejs
 
-sudo dnf -y update
+dnf -y update
 
 # Fonts
-sudo dnf -y install wqy*
-sudo dnf install -y cjkuni-ukai-fonts cjkuni-uming-fonts font-manager
+dnf -y install wqy*
+dnf install -y cjkuni-ukai-fonts cjkuni-uming-fonts font-manager
 
 # Gpaste Integration
-sudo dnf install gnome-shell-extension-gpaste -y
+dnf install gnome-shell-extension-gpaste -y
 
 # Must
-sudo dnf -y install gnome-tweak-tool libreoffice-langpack-zh-Hant
+dnf -y install gnome-tweak-tool libreoffice-langpack-zh-Hant
 
 # Editor
-sudo dnf install -y vim
+dnf install -y vim
 wget https://atom.io/download/rpm /tmp/atom.rpm
-sudo dnf install -y /tmp/atom.rpm
+dnf install -y /tmp/atom.rpm
 
 # Dev
-sudo dnf install -y gcc gcc-c++ git tig ctags
-sudo dnf install -y eclipse
-sudo dnf install -y nodejs npm
-sudo npm install surge -g
-sudo dnf install -y python3
-sudo dnf install -y ruby ruby-devel rubygems
-sudo gem install github-pages --no-ri --no-rdoc
-sudo gem install bundle --no-ri --no-rdoc
+dnf install -y gcc gcc-c++ git tig ctags
+dnf install -y eclipse
+dnf install -y nodejs npm
+npm install surge -g
+dnf install -y python3
+dnf install -y ruby ruby-devel rubygems
+gem install github-pages --no-ri --no-rdoc
+gem install bundle --no-ri --no-rdoc
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p # FYI: https://github.com/guard/listen/wiki/Increasing-the-amount-of-inotify-watchers
-sudo dnf -y install codeblocks
+dnf -y install codeblocks
 
-sudo dnf install -y compat-libstdc++-296.i686 compat-libstdc++-33.i686 compat-libstdc++-33.x86_64 ncurses-libs.i686 # for android studio
-sudo dnf install -y https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm # google-chrome
-sudo dnf install -y gimp inkscape mypaint # Draw
+dnf install -y compat-libstdc++-296.i686 compat-libstdc++-33.i686 compat-libstdc++-33.x86_64 ncurses-libs.i686 # for android studio
+dnf install -y https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm # google-chrome
+dnf install -y gimp inkscape mypaint # Draw
 
 # Input Method
-sudo dnf install -y fcitx fcitx-table-chinese fcitx-configtool fcitx-qt4 fcitx-chewing im-chooser
+dnf install -y fcitx fcitx-table-chinese fcitx-configtool fcitx-qt4 fcitx-chewing im-chooser
 gsettings set org.gnome.settings-daemon.plugins.keyboard active false
-sudo echo "export GTK_IM_MODULE=fcitx" >> /etc/profile
-sudo echo "export QT_IM_MODULE=fcitx" >> /etc/profile
-sudo echo "export XMODIFIERS="@im=fcitx"" >> /etc/profile
+echo "export GTK_IM_MODULE=fcitx" >> /etc/profile
+echo "export QT_IM_MODULE=fcitx" >> /etc/profile
+echo "export XMODIFIERS="@im=fcitx"" >> /etc/profile
 im-chooser
 
 # clone conf files
