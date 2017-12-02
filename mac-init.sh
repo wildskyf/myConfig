3d75acd70ea89b1776fc6c1186b5f311b3e27dc1
wildsky <poppin.wildsky+gh@gmail.com>
2017-12-02 20:38:45 +0800

    Update mac-init.sh

--- mac-init.sh
+++ mac-init.sh
@@ -5,13 +5,7 @@ echo "Install Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "Install dev tools"
-brew install wget gcc gpp gpg
-
-# echo "Install rvm"
-# gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-# \curl -sSL https://get.rvm.io | bash -s stable
-
-echo "think about install rbenv..."
+brew install wget gcc gpp gpg rbenv
 
 echo "Install latest node.js"
 curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
@@ -20,8 +14,8 @@ echo "Install Caskroom"
 brew tap caskroom/cask
 
 echo "Install Applications..."
-brew cask install firefox google-chrome nally iterm2 sequelpro \
-     telegram evernote colorpicker colorpicker-hex colorpicker-developer appcleaner slack keka
+brew cask install firefox google-chrome nally iterm2 sequel-pro \
+     telegram evernote colorpicker-developer appcleaner slack keka quip
 
 echo "Install Fonts..."
 brew tap caskroom/fonts
@@ -32,7 +26,7 @@ brew cask install font-inconsolata-dz-for-powerline
 # install QuickLook
 # https://github.com/sindresorhus/quick-look-plugins
 echo "Install QuoickLook Plugins..."
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql suspicious-package ipaql
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql suspicious-package
 
 
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
