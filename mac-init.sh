526c35391c9b97b773216987adbd51a337aaaf9b
wildsky <poppin.wildsky+gh@gmail.com>
2017-12-02 16:52:05 +0800

    Update mac-init.sh

--- mac-init.sh
+++ mac-init.sh
@@ -7,9 +7,11 @@ echo "Install Homebrew"
 echo "Install dev tools"
 brew install wget gcc gpp gpg
 
-echo "Install rvm"
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-\curl -sSL https://get.rvm.io | bash -s stable
+# echo "Install rvm"
+# gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+# \curl -sSL https://get.rvm.io | bash -s stable
+
+echo "think about install rbenv..."
 
 echo "Install latest node.js"
 curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
@@ -32,6 +34,16 @@ brew cask install font-inconsolata-dz-for-powerline
 echo "Install QuoickLook Plugins..."
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql suspicious-package ipaql
 
+
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh
+
 echo "=========== FINISHED ==========="
 echo "Ref: http://blog.visioncan.com/2014/introducing-cask/"
+
+
+
+
+
+
 
