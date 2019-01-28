39ab4e62fb5bd299c0f5379701327e988abf30b2
wildsky <poppin.wildsky+gh@gmail.com>
2019-01-28 16:31:32 +0800

    Update alias.bash

--- .bash.d/alias.bash
+++ .bash.d/alias.bash
@@ -1,19 +1,27 @@
-alias gf='git flow'
-alias gff='git flow feature'
 alias ll='ls -l'
 alias la='ls -a'
 alias dir='ls -ba'
 alias c='clear'
 
 alias vi='vim -u ~/.vim.d/general.vimrc'
+alias g='git'
+alias gut='git'
+alias gs='git status'
+alias gc='git commit'
+alias gd='git diff'
+alias gco='git checkout'
+alias ga='git add'
+alias gp='git pull --rebase'
+alias gpo='git pull --rebase origin'
+alias ts='tig status'
+
+alias cat='bat'
 
 # alias runnvm='source $(brew --prefix nvm)/nvm.sh'
 # alias 'uninstall pow'='curl get.pow.cx/uninstall.sh | sh'
 # alias 'clean homebrew'='brew cleanup -s'
-# alias vim='emacsclient -nw'
-# alias emacs='emacs -nw'
-alias emacs='emacsclient -nw'
-alias emacserver='\emacs --daemon > /dev/null'
-alias killemacs="emacsclient -e '(kill-emacs)'"
-# alias runmattermost='docker run --name mattermost-preview -d --publish 8065:8065 mattermost/platform'
+
+
+alias :q='exit'
+alias :x='exit'
 
