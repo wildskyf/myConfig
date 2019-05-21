68dabd068a162082af6591beaafd4822861153e3
wildsky <poppin.wildsky+gh@gmail.com>
2019-05-21 09:51:39 +0800

    Update mac-init.sh

--- mac-init.sh
+++ mac-init.sh
@@ -5,7 +5,7 @@ echo "Install Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "Install dev tools"
-brew install wget gcc gpp gpg rbenv rbenv-gemset
+brew install wget gcc gpp gpg rbenv rbenv-gemset ripgrep
 
 echo "Install latest node.js"
 curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
